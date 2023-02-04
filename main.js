62916ec23c037771ed52da2c90c0bf5db65c44dc
christinahe <he.chri@northeastern.edu>
2023-02-04 15:20:24 -0500

    vis 1 description

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
+// initializes color variable to black
+let color = 'black';
+
 // function for when button is clicked
 function buttonClicked() {
     // select the element
-    let color = document.body.style.color;
     if (color == 'black') {
         document.body.style.color = "rgb(200,40,140)";
     }
     else {
         document.body.style.color = 'black';
     }
+    color = document.body.style.color;
 }
